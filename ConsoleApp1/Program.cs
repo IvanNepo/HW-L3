2fc0bf3113cea8f6bb55508e2b79abc7068e3a62
IvanNepo <156950275+IvanNepo@users.noreply.github.com>
2024-01-29 19:13:55 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HW3
+{
+    internal class EnumDemo 
+    {
+        enum month { Janury,February,March,April,May,June,July,August,September,October,November,December};
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter month number: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 1 || n > 12)
+                Console.WriteLine("Incorrect number");
+        else
+                Console.WriteLine("Month at number {0}: {1}", n, (month)(n - 1));
+            Console.ReadKey();
+                }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Month;
+            Console.WriteLine("Month number: ");
+            Month = int.Parse(Console.ReadLine());
+            switch (Month)
+            {       case 1:
+                    case 2:
+                    case 12:
+                    Console.WriteLine("Winter");
+                    break;
+                    case 3:
+                    case 4:
+                    case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                    case 6:
+                    case 7:
+                    case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                    case 9:
+                    case 10:
+                    case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
